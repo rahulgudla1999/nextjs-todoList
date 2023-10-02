a9ae2bc24478ddd4c7ff8041f2c35dc9b08b3a59
Rahul Gudla <rahulgudla1999@gmail.com>
2023-10-02 14:03:22 +0530

    crud toDOList

--- src/app/api/topics/route.js
+++ src/app/api/topics/route.js
@@ -1,10 +1,23 @@
 import { NextResponse } from "next/server";
-import connectMongoDB from "../../../libs/mongodb"
-import Topic from "../../../models/topic";
+import connectMongoDB from "@/libs/mongodb"
+import Topic from "@/models/topic";
 
-export default async function POST(request){
+export async function POST(request){
     const {title, description} = await request.json();
     await connectMongoDB();
     await Topic.create({title, description});
     return NextResponse.json({message:"Topic Created", success:true},{status:201});
+}
+
+export async function GET(){
+    await connectMongoDB();
+    const topics = await Topic.find();
+    return NextResponse.json({topics});
+}
+
+export async function DELETE(request) {
+   const id = request.nextUrl.searchParams.get("id");
+   await connectMongoDB();
+   await Topic.findByIdAndDelete(id);
+   return NextResponse.json({message: "Topic deleted Successfully"}, {status:200});
 }
